93091669e75f39814b99a8dad2a1f42950c1edc9
Zodiac Software <86096723+ZS-cop@users.noreply.github.com>
2022-06-01 00:33:50 +0530

    master

--- javascript/main.js
+++ javascript/main.js
@@ -18,14 +18,15 @@ let isDrawing;
 
 const main_canvas = get("#main_canvas");
 window.addEventListener("resize", () => {
-  main_canvas.height = window.innerHeight;
-  main_canvas.width = window.innerWidth;
+  main_canvas.height = window.innerHeight - main_canvas.style.top - 15;
+  main_canvas.width = window.innerWidth - main_canvas.style.left - 105;
   main_ctx.beginPath();
   main_ctx.fillStyle = "rgb(247 245 245)";
   main_ctx.rect(0, 0, main_canvas.width, main_canvas.height);
-  main_ctx.fill();});
-main_canvas.height = window.innerHeight;
-main_canvas.width = window.innerWidth;
+  main_ctx.fill();
+});
+main_canvas.height = window.innerHeight - main_canvas.style.top - 15;
+main_canvas.width = window.innerWidth - main_canvas.style.left - 105;
 const main_ctx = main_canvas.getContext("2d");
 
 main_ctx.beginPath();
@@ -37,17 +38,23 @@ ct = "none";
 
 let pencil = (el, ctx, color, width) => {
   el.onpointerdown = function (e) {
+    const rect = main_canvas.getBoundingClientRect();
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
     ctx.beginPath();
     isDrawing = true;
     ctx.lineWidth = width;
     ctx.lineCap = "round";
-    ctx.moveTo(e.clientX, e.clientY);
+    ctx.moveTo(x, y);
   };
   el.onpointermove = function (e) {
     if (isDrawing) {
+      const rect = main_canvas.getBoundingClientRect();
+      x = e.clientX - rect.left;
+      y = e.clientY - rect.top;
       console.log(width, color);
       ctx.strokeStyle = color;
-      ctx.lineTo(e.clientX, e.clientY);
+      ctx.lineTo(x, y);
       ctx.stroke();
     }
   };
@@ -58,19 +65,25 @@ let pencil = (el, ctx, color, width) => {
 
 let Clay = (el, ctx, color, width, blur = 0) => {
   el.onpointerdown = function (e) {
+    const rect = main_canvas.getBoundingClientRect();
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
     isDrawing = true;
     ctx.lineWidth = width;
     ctx.lineCap = "round";
     ctx.filter = `blur(${blur}px)`;
     // ctx.shadowColor = color;
-    ctx.moveTo(e.clientX, e.clientY);
+    ctx.moveTo(x, y);
   };
   el.onpointermove = function (e) {
     if (isDrawing) {
+      const rect = main_canvas.getBoundingClientRect();
+      x = e.clientX - rect.left;
+      y = e.clientY - rect.top;
       ctx.beginPath();
       ctx.filter = `blur(${blur}px)`;
       ctx.strokeStyle = color;
-      ctx.lineTo(e.clientX, e.clientY);
+      ctx.lineTo(x, y);
       ctx.stroke();
     }
   };
@@ -85,13 +98,16 @@ let Circle = (el, ctx, color, width, spread) => {
   };
   el.onpointermove = function (e) {
     if (isDrawing) {
+      const rect = main_canvas.getBoundingClientRect();
+      x = e.clientX - rect.left;
+      y = e.clientY - rect.top;
       for (i = 0; i < 4; i++) {
         ctx.beginPath();
         ctx.fillStyle = color;
         ctx.globalAlpha = Math.random();
         ctx.arc(
-          getRandomInt(e.clientX, e.clientX),
-          getRandomInt(e.clientY, e.clientY + spread),
+          getRandomInt(x, x),
+          getRandomInt(y, y + spread),
           getRandomInt(width, width + 2),
           0 * Math.PI,
           2 * Math.PI
@@ -105,18 +121,39 @@ let Circle = (el, ctx, color, width, spread) => {
   };
 };
 
-let click = (btn, box) => {
-  document.getElementById(btn).addEventListener("click", () => {
-    if (document.getElementById(box).style.display != "none") {
-      document.getElementById(box).style.display = "none";
-    } else {
-      document.getElementById(box).style.display = "block";
-    }
-  });
+let click = (btn, box, btns = false) => {
+  if ((btns = false)) {
+    document.getElementById(btn).addEventListener("click", () => {
+      document.querySelectorAll(".btn").forEach((e) => {
+        e.style.backgroundColor = "rgba(87, 87, 87, 0.5)";
+        e.style.borderRadius = "2rem";
+      });
+      if (document.getElementById(box).style.display != "none") {
+        document.getElementById(btn).style.backgroundColor =
+          "rgba(87, 87, 87, 0.5)";
+        document.getElementById(btn).style.borderRadius = "2rem";
+        document.getElementById(box).style.display = "none";
+      } else {
+        document.querySelectorAll(".toolbox div").forEach((e) => {
+          e.style.display = "none";
+        });
+        document.getElementById(btn).style.backgroundColor = "#90ee90";
+        document.getElementById(btn).style.borderRadius = "10px";
+        document.getElementById(box).style.display = "block";
+      }
+    });
+  } else {
+    document.getElementById(btn).addEventListener("click", () => {
+      if (document.getElementById(box).style.display != "none") {
+        document.getElementById(box).style.display = "none";
+      } else {
+        document.getElementById(box).style.display = "block";
+      }
+    });
+  }
 };
 
 click("brush_btn", "brush_tool");
-click("b_bt", "brush_tool");
 
 var defaultPicker = new iro.ColorPicker("#color_picker", {
   width: 280,
@@ -162,9 +199,9 @@ get("#width_p").addEventListener("change", () => {
     get("#width_p").value
   );
 });
-click("get_color", "color_picker");
-click("get_colorc", "color_picker");
-click("get_colorcr", "color_picker");
+click("get_color", "color_picker", true);
+click("get_colorc", "color_picker", true);
+click("get_colorcr", "color_picker", true);
 
 document.getElementById("color_text").addEventListener("input", () => {
   document.getElementById("color_text").addEventListener("keypress", (e) => {
@@ -211,21 +248,22 @@ document.getElementById("width_e").addEventListener("input", () => {
   };
   main_canvas.onmousemove = function (e) {
     if (isDrawing) {
+      const rect = main_canvas.getBoundingClientRect();
+      x = e.clientX - rect.left;
+      y = e.clientY - rect.top;
       main_ctx.clearRect(
-        e.clientX,
-        e.clientY,
+        x,
+        y,
         get("#width_e").value,
         get("#width_e").value
       );
     }
   };
   main_canvas.onmouseup = function (e) {
-    main_ctx.clearRect(
-      e.clientX,
-      e.clientY,
-      get("#width_e").value,
-      get("#width_e").value
-    );
+    const rect = main_canvas.getBoundingClientRect();
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    main_ctx.clearRect(x, y, get("#width_e").value, get("#width_e").value);
     isDrawing = false;
   };
   main_canvas.addEventListener("touchmove", function (e) {
@@ -236,3 +274,5 @@ document.getElementById("width_e").addEventListener("input", () => {
     main_ctx.clearRect(x, y, get("#width_e").value, get("#width_e").value);
   });
 });
+
+click("layers_btn", "layers");
